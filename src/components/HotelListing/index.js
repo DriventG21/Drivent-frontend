df793b21d6febe9d541a05e4e1b573f842776c82
Viniccius Wirz <vinimovel23@gmail.com>
2023-08-01 17:57:45 -0300

    feat: hotel listing error handler

--- src/components/HotelListing/index.js
+++ src/components/HotelListing/index.js
@@ -5,22 +5,48 @@ import HotelItem from './HotelItem';
 
 export default function HotelListing() {
   const [data, setData] = useState([]);
-  const { hotels } = useHotel();
+  const [error, setError] = useState(null);
+  const hotelsData = useHotel();
+  const { hotels, hotelsError } = hotelsData;
 
   useEffect(() => {
     if (hotels) {
       setData(hotels);
     }
-  }, [hotels]);
+    if (hotelsError) {
+      const data = hotelsError.response.data;
+      const errorMessage = {
+        'Payment Required': ['Você precisa ter confirmado pagamento antes de fazer a escolha de hospedagem'],
+        'Invalid ticket type': [
+          'Sua modalidade de ingresso não inclui hospedagem',
+          'Prossiga para a escolha de atividades',
+        ],
+      };
+      setError(errorMessage[data]);
+    }
+  }, [hotels, hotelsError]);
 
   return (
     <PageContainer>
-      <h2>Primeiro, escolha seu hotel</h2>
-      <ul>
-        {data && data.map((h) => (
-          <HotelItem key={h.id} hotelInfo={h} />
-        ))}
-      </ul>
+      {hotels && (
+        <>
+          <h2>Primeiro, escolha seu hotel</h2>
+          <ul>
+            {data.map((h) => (
+              <HotelItem key={h.id} hotelInfo={h} />
+            ))}
+          </ul>
+        </>
+      )}
+      {error && (
+        <ErrorContainer>
+          <ul>
+            {error.map((e) => (
+              <div>{e}</div>
+            ))}
+          </ul>
+        </ErrorContainer>
+      )}
     </PageContainer>
   );
 }
@@ -54,5 +80,26 @@ const PageContainer = styled.div`
       background-color: #c9c6c6;
       border-radius: 4px;
     }
+  }
+`;
+
+const ErrorContainer = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 23px;
+  letter-spacing: -0.05em;
+  text-align: center;
+  color: #8e8e8e;
+  align-self: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 48%;
+  height: 500px;
+  ul {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 0px;
   }
 `;
